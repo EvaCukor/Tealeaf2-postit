4641f16a3e3374cdd62f87b85678b9634b3bb7bf
EvaCukor <evacukor@gmail.com>
2015-02-18 18:34:00 +0000

    added user-related functions and authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :require_user
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment.user = User.first
+    @comment.user = current_user
     
     if @comment.save
-      flash[:note] = "Your comment was added."
+      flash[:notice] = "Your comment was added."
       redirect_to post_path(@post)
     else  
       render 'posts/show'
